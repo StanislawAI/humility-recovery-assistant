974dbf996f6af98d0ec256e5c1184caa5a1b6c07
StanislawAI <sortonowski@gmail.com>
2025-10-16 19:20:53 +0200

    Update dashboard page and add backup version

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -62,8 +62,6 @@ size="lg"
 
       <AIAdvisor />
 
-      <EmergencyCard />
-
 <div className="grid gap-8 lg:grid-cols-2">
   <div className="space-y-8">
     <Card>
@@ -146,6 +144,8 @@ size="lg"
       <TriggersTracker />
       <ServiceRoster />
       </div>
+
+      <EmergencyCard />
 
       <CravingModal open={showCravingModal} onOpenChange={setShowCravingModal} />
     </div>
